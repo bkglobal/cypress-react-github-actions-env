5f800568d525892cc7cb8406f1a4a61558dfed97
Bilal khatri <khatribilal5@gmail.com>
2023-09-11 12:46:43 +0500

    Added record as well

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 require("dotenv").config();
 
 module.exports = defineConfig({
+  projectId: "vdytfu",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
