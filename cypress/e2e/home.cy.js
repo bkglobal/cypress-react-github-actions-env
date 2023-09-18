a912f428b8a28ef8844a25962bc17be6dcff05a6
Bilal khatri <khatribilal5@gmail.com>
2023-09-19 01:21:27 +0500

    logo app

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -7,5 +7,6 @@ describe("template spec", () => {
     console.log("CYPRESS TEST CASES ======>    ENV SECS ARE .. ", Cypress.env("base_url"), Cypress.env("auth0_username"), Cypress.env("auth0_password"));
     cy.get('[data-cy="click-btn"]').click();
     cy.get('[data-cy="baseUrlInput"]').type(Cypress.env("base_url"));
+    cy.get('[data-cy="logo-app"]').should('exist');
   });
 });
--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" data-cy="logo-app"/>
         <p>
           {process.env.REACT_APP_BACKEND_URL}
         </p>
